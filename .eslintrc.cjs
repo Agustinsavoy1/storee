0e429acdedf048f758e2580d2f5c35aa06a7e3eb
chakkyy <cmramirez1996@gmail.com>
2023-08-25 19:14:24 -0300

    feat: added eslint and prettier rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,6 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
@@ -15,4 +14,89 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+};
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    es2020: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+      arrowFunctions: true,
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  plugins: [
+    'react',
+    'react-refresh',
+    '@typescript-eslint',
+    'prettier',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        paths: ['./src', './scripts'],
+      },
+    },
+  },
+  rules: {
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: ['function-declaration', 'arrow-function'],
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/require-default-props': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'no-restricted-exports': 'off',
+    'react/no-unstable-nested-components': 'off',
+    '@typescript-eslint/require-await': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/restrict-plus-operands': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+    'react/no-unused-prop-types': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    'no-param-reassign': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'react/prop-types': 'off',
+    'react/no-array-index-key': 'off',
+  },
+};
